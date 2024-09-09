8e1465f74189d5f81f1b09438e6090c50f8e818e
Anupam Tiwari <anupamtiwari8090@gmail.com>
2024-09-09 23:41:46 +0530

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
-
-import Navigation from "./components/navigation";
+import Navigation from "./components/Navigation";
 import HeroSection from "./components/Hero";
+
 const App = () => {
   return (
     <div>
