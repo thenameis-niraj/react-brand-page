30c7f30ed903c92a3741cbba4517a37d42f64570
Anupam Tiwari <anupamtiwari8090@gmail.com>
2024-09-09 23:34:52 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+import Navigation from "./components/navigation";
+import HeroSection from "./components/Hero";
+const App = () => {
+  return (
+    <div>
+      <Navigation />;
+      <HeroSection />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,19 @@
+const Navigation = () => {
+  return (
+    <div>
+      <nav className="container">
+        <div className="logo">
+          <img src="/images/brand_logo.png" alt="logo" srcset="" />
+        </div>
+        <ul>
+          <li href="#">Menu</li>
+          <li href="#">Location</li>
+          <li href="#">Contact</li>
+          <li href="#">About</li>
+        </ul>
+        <button>Login</button>
+      </nav>
+    </div>
+  );
+};
+export default Navigation;
